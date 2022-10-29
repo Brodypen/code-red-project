92051653f9831a3b1d4439c3f692e73510ecec01
FrostandIce <brodypen7987@gmail.com>
2022-10-29 15:37:55 -0500

    new push

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,16 +1,29 @@
 import React from 'react'
 import {
   VStack,
+  Flex,
+  HStack,
   Heading,
   Text,
+  Box,
 } from '@chakra-ui/react';
 const MainPage = () => {
   return (
-    <VStack spacing={4}>
-      <Heading>Home</Heading>
-      <Text>Brody is here!</Text>
-      <Text>swag!</Text>
-    </VStack>
+    <Flex direction="column" justify="center" padding={5}>
+      <VStack>
+        <Heading>Home</Heading>
+      </VStack>
+      <HStack paddingTop={5} paddingLeft={20} paddingRight={20}>
+        <VStack>
+          <Box>
+            <Text fontSize="4xl">Wish you know?</Text>
+            <Text fontSize="4xl">Get it here!</Text>
+            <Text fontSize="4xl">Search it now</Text>
+            <Text fontSize="xl">Search it now</Text>
+          </Box>
+        </VStack>
+      </HStack>
+    </Flex>
   );
 }
 
